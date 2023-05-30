ffbad8428e04209c355aa1e69a60b98f6fb9a0fd
gateremark <mark5gatere@gmail.com>
2023-05-30 18:08:18 +0300

    removes navigate console

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,6 @@ const Navbar = () => {
 	};
 
 	const navigate = useNavigate();
-	console.log(navigate);
 	return (
 		<div className="bg-navcolor/50 shadow-md sticky top-0 z-99">
 			<header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
@@ -31,7 +30,7 @@ const Navbar = () => {
 				<div>
 					<ul className="flex space-x-10">
 						<li
-							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] ${
+							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] hover:text-[#9dd8eb] ${
 								pathRoute("/") && "border-b-[3px] text-[#9dd8eb]"
 							} `}
 							onClick={() => navigate("/")}
@@ -39,7 +38,7 @@ const Navbar = () => {
 							Home
 						</li>
 						<li
-							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] ${
+							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] hover:text-[#9dd8eb] ${
 								pathRoute("/offers") && "border-b-[3px] text-[#9dd8eb]"
 							} `}
 							onClick={() => navigate("/offers")}
@@ -47,7 +46,7 @@ const Navbar = () => {
 							Offers
 						</li>
 						<li
-							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] ${
+							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] hover:text-[#9dd8eb] ${
 								pathRoute("/sign-in") && "border-b-[3px] text-[#9dd8eb]"
 							} `}
 							onClick={() => navigate("/sign-in")}
@@ -55,14 +54,14 @@ const Navbar = () => {
 							Sign In
 						</li>
 						<li
-							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] ${
+							className={`cursor-pointc py-3 text-base font-semibold text-[#18C7FA] hover:text-[#9dd8eb] ${
 								pathRoute("/sign-up") && "border-b-[3px] text-[#9dd8eb]"
 							} `}
 							onClick={() => navigate("/sign-up")}
 						>
 							Sign Up
 						</li>
-						<li className="cursor-pointc py-3 text-xl font-semibold text-[#334155] items-center">
+						<li className="cursor-pointc py-3 text-xl font-semibold text-[#334155] items-center hover:text-[#9dd8eb] focus:outline-none focus:ring-offset-2">
 							<a href="https://github.com/gateremark/hometopia" target="_blank">
 								<FaGithub />
 							</a>
@@ -70,6 +69,8 @@ const Navbar = () => {
 					</ul>
 				</div>
 			</header>
+
+			{/* Mobile View */}
 		</div>
 	);
 };
