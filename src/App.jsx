55756d94185b4c1d66f7aeb7553edf84c6fed06b
gateremark <mark5gatere@gmail.com>
2023-05-30 03:00:24 +0300

    Added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Logo from './assets/logo/logo-nobg.png'
+import Home from "./assets/homes/home1.png";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		<>
+			<div>
+				<a href="https://gateremark.hometopia" target="_blank">
+					<img src={Logo} className="logo" alt="Main logo" />
+				</a>
+			</div>
+			{/* <img src={Home} className="logo" alt="Home" /> */}
+      <h1 className=' text-center'>Hello Here!</h1>
+		</>
+	);
 }
 
 export default App
