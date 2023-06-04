021d3d734f10e2db3e4751ee8e2105dc9768bedd
gateremark <mark5gatere@gmail.com>
2023-06-04 13:31:02 +0300

    Added the moment package for timestamp

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,9 +1,12 @@
 // import Moment from "react-moment"
+import moment from "moment";
 import { Link } from "react-router-dom";
 import { MdLocationPin, MdEdit, MdDelete } from "react-icons/md";
 import { FaBath, FaBed } from "react-icons/fa";
 
 const ListingItem = ({ listing, id, onEdit, onDelete }) => {
+	const realTime = listing.timestamp;
+	const date = realTime.toDate();
 	return (
 		<li className="relative bg-[#e2e2e2] flex flex-col justify-between item-center shadow-lg hover:shadow-xl rounded overflow-hidden transition-shadow duration-150 mx-[10px] mb-4">
 			<Link className="contents" to={`/category/${listing.type}/${id}`}>
@@ -16,9 +19,9 @@ const ListingItem = ({ listing, id, onEdit, onDelete }) => {
 
 				{/* <Moment fromNow>{listing.timestamp?.toDate()}</Moment> */}
 				{/* <Moment>1976-04-19T12:59-0500</Moment> */}
-
 				<p className="absolute top-2 left-2 bg-[#10192D] text-[#e2e2e2] uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg">
-					4 Hours Ago
+					{moment(date).format("Do MMMM YYYY, h:mm a")}
+					
 				</p>
 				<div className="w-full p-3 relative">
 					<div className="flex items-center gap-1">
