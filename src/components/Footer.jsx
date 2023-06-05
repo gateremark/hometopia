c6a39e18a5ab697430ea6d40ac1e25e59458879f
gateremark <mark5gatere@gmail.com>
2023-06-06 00:43:19 +0300

    worked on the home page and footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,123 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../index.css";
+import {
+	IoLogoGithub,
+	IoLogoTwitter,
+	IoLogoLinkedin,
+	IoLogoInstagram,
+} from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const Footer = () => {
-	return <div></div>;
+	const [pageState, setPageState] = useState("Sign In");
+	const navigate = useNavigate();
+	const auth = getAuth();
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setPageState("Profile");
+			} else {
+				setPageState("Sign In");
+			}
+		});
+	}, [auth]);
+	return (
+		<div
+			className="relative w-[100%] min-h-[100px] px-5 py-12 flex justify-center items-center flex-col bg-[#3586ff]"
+			id="waves"
+		>
+			<div className="">
+				<div
+					className="absolute top-[-100px] left-0 w-[100%] h-[100px]  z-1000 opacity-100 bottom-0"
+					id="wave1"
+				></div>
+				<div
+					className="absolute top-[-100px] left-0 w-[100%] h-[100px] z-999 opacity-50 bottom-[10px]"
+					id="wave2"
+				></div>
+				<div
+					className="absolute top-[-100px] left-0 w-[100%] h-[100px] z-1000 opacity-20 bottom-[15px]"
+					id="wave3"
+				></div>
+				<div
+					className="absolute top-[-100px] left-0 w-[100%] h-[100px] z-999 opacity-70 bottom-[20px]"
+					id="wave4"
+				></div>
+			</div>
+			<div className="relative flex justify-center items-center my-2.5 mx-0 flex-wrap">
+				<a
+					href="https://github.com/gateremark"
+					className="text-[#e2e2e2] text-3xl my-0 mx-[10px] inline-block transition duration-500 ease-in-out hover:translate-y-[-10px] cursor-pointc"
+					target="_blank"
+				>
+					{" "}
+					<IoLogoGithub />{" "}
+				</a>
+				<a
+					href="https://twitter.com/gatere_mark"
+					className="text-[#e2e2e2] text-3xl my-0 mx-[10px] inline-block transition duration-500 ease-in-out hover:translate-y-[-10px] cursor-pointc"
+					target="_blank"
+				>
+					{" "}
+					<IoLogoTwitter />{" "}
+				</a>
+				<a
+					href="https://www.linkedin.com/in/gateremark/"
+					className="text-[#e2e2e2] text-3xl my-0 mx-[10px] inline-block transition duration-500 ease-in-out hover:translate-y-[-10px] cursor-pointc"
+					target="_blank"
+				>
+					{" "}
+					<IoLogoLinkedin />{" "}
+				</a>
+				<a
+					href="https://www.instagram.com/gateremark/"
+					className="text-[#e2e2e2] text-3xl my-0 mx-[10px] inline-block transition duration-500 ease-in-out hover:translate-y-[-10px] cursor-pointc"
+					target="_blank"
+				>
+					{" "}
+					<IoLogoInstagram />{" "}
+				</a>
+			</div>
+			<ul className="relative text-[#e2e2e2] gap-5 text-lg flex justify-center items-center mt-4 mx-0 flex-wrap">
+				<li
+					className="hover:text-[#001f41] transition duration-300 ease-in-out cursor-pointc"
+					onClick={() => navigate("/")}
+				>
+					Home
+				</li>
+				<li
+					className="hover:text-[#001f41] transition duration-300 ease-in-out cursor-pointc"
+					onClick={() => navigate("/offers")}
+				>
+					Offers
+				</li>
+				<li
+					className="hover:text-[#001f41] transition duration-300 ease-in-out cursor-pointc"
+					onClick={() => navigate("/profile")}
+				>
+					{pageState}
+				</li>
+				{pageState !== "Profile" && (
+					<li
+						className="hover:text-[#001f41] transition duration-300 ease-in-out cursor-pointc"
+						onClick={() => navigate("/sign-up")}
+					>
+						Sign Up
+					</li>
+				)}
+			</ul>
+			<p className="mt-10 mb-2 text-base text-[#e2e2e2] text-center">
+				&copy; 2023 HomeTopia | All Rights Reserved | by{" "}
+				<span className="hover:text-[#001f41] transition duration-300 ease-in-out">
+					{" "}
+					<a href="https://gateremark.vercel.app/" target="_blank">
+						@gateremark
+					</a>{" "}
+				</span>
+			</p>
+		</div>
+	);
 };
 
 export default Footer;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,204 @@
+import { useEffect, useState } from "react";
 import HomeSlider from "../components/HomeSlider";
+import {
+	collection,
+	getDocs,
+	limit,
+	orderBy,
+	query,
+	where,
+} from "firebase/firestore";
+import { db } from "../firebase";
+// import { toast } from "react-toastify";
+import { CiLocationArrow1 } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
+import Footer from "../components/Footer";
 
 const Home = () => {
-	return <div>
-		<HomeSlider />
+	// Fetching data for offers
+	const [offerListings, setOfferListings] = useState(null);
 
-	</div>;
+	useEffect(() => {
+		const fetchListings = async () => {
+			try {
+				// get reference
+				const listingsRef = collection(db, "listings");
+				// create a query
+				const q = query(
+					listingsRef,
+					where("offer", "==", true),
+					orderBy("timestamp", "desc"),
+					limit(4),
+				);
+				// execute a query
+				const querySnap = await getDocs(q);
+				const listings = [];
+				querySnap.forEach((doc) => {
+					return listings.push({
+						id: doc.id,
+						data: doc.data(),
+					});
+				});
+				setOfferListings(listings);
+				// console.log(listings);
+			} catch (error) {
+				console.log(error);
+				// toast.error("Could not load properties 'On Offer'")
+			}
+		};
+		fetchListings();
+	}, []);
+
+	// Places for rent
+	const [rentListings, setRentListings] = useState(null);
+	useEffect(() => {
+		const fetchListings = async () => {
+			try {
+				// get reference
+				const listingsRef = collection(db, "listings");
+				// create the query
+				const q = query(
+					listingsRef,
+					where("type", "==", "rent"),
+					orderBy("timestamp", "desc"),
+					limit(4),
+				);
+				// execute the query
+				const querySnap = await getDocs(q);
+				const listings = [];
+				querySnap.forEach((doc) => {
+					return listings.push({
+						id: doc.id,
+						data: doc.data(),
+					});
+				});
+				setRentListings(listings);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchListings();
+	}, []);
+	// Places for sales
+	const [saleListings, setSaleListings] = useState(null);
+	useEffect(() => {
+		async function fetchListings() {
+			try {
+				// get reference
+				const listingsRef = collection(db, "listings");
+				// create the query
+				const q = query(
+					listingsRef,
+					where("type", "==", "sale"),
+					orderBy("timestamp", "desc"),
+					limit(4),
+				);
+				// execute the query
+				const querySnap = await getDocs(q);
+				const listings = [];
+				querySnap.forEach((doc) => {
+					return listings.push({
+						id: doc.id,
+						data: doc.data(),
+					});
+				});
+				setSaleListings(listings);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchListings();
+	}, []);
+
+	return (
+		<div>
+			<HomeSlider />
+			<div className="max-w-6xl mx-auto pt-4 space-y-6 mt-6 mb-28">
+				{offerListings && offerListings.length > 0 && (
+					<div className="">
+						<div className="mx-4 mb-6 flex justify-between items-center">
+							<h2 className="px-3 text-2xl font-semibold text-[#10192D]">
+								On Offer 🎉
+							</h2>
+							<Link
+								to="/offers"
+								className="flex items-center gap-1 text-[#004792] hover:text-[#001f41] transition duration-150 ease-in-out"
+							>
+								<CiLocationArrow1 className="text-2xl" />
+								<p className=" text-lg">More Offers</p>
+							</Link>
+						</div>
+
+						<ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+							{offerListings.map((listing) => (
+								<ListingItem
+									key={listing.id}
+									listing={listing.data}
+									id={listing.id}
+								/>
+							))}
+						</ul>
+					</div>
+				)}
+
+				{rentListings && rentListings.length > 0 && (
+					<div className="">
+						<div className="mx-4 mb-6 flex justify-between items-center">
+							<h2 className="px-3 text-2xl font-semibold text-[#10192D]">
+								Rentals
+							</h2>
+							<Link
+								to="/category/rent"
+								className="flex items-center gap-1 text-[#004792] hover:text-[#001f41] transition duration-150 ease-in-out"
+							>
+								<CiLocationArrow1 className="text-2xl" />
+								<p className=" text-lg">More Rentals</p>
+							</Link>
+						</div>
+
+						<ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+							{rentListings.map((listing) => (
+								<ListingItem
+									key={listing.id}
+									listing={listing.data}
+									id={listing.id}
+								/>
+							))}
+						</ul>
+					</div>
+				)}
+
+				{saleListings && saleListings.length > 0 && (
+					<div className="">
+						<div className="mx-4 mb-6 flex justify-between items-center">
+							<h2 className="px-3 text-2xl font-semibold text-[#10192D]">
+								On Sale
+							</h2>
+							<Link
+								to="/category/sale"
+								className="flex items-center gap-1 text-[#004792] hover:text-[#001f41] transition duration-150 ease-in-out"
+							>
+								<CiLocationArrow1 className="text-2xl" />
+								<p className=" text-lg">More On Sale</p>
+							</Link>
+						</div>
+
+						<ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+							{saleListings.map((listing) => (
+								<ListingItem
+									key={listing.id}
+									listing={listing.data}
+									id={listing.id}
+								/>
+							))}
+						</ul>
+					</div>
+				)}
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
