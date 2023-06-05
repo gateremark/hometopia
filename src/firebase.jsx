7dbbb747be700e7f411dc575a42c83f71aa9cd2e
gateremark <mark5gatere@gmail.com>
2023-06-06 00:54:38 +0300

    Added firebase analytics

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,8 +13,10 @@ const firebaseConfig = {
 	storageBucket: "hometopia-d224f.appspot.com",
 	messagingSenderId: "471415467397",
 	appId: "1:471415467397:web:67b0d3c0122332a384e8ec",
+	measurementId: "G-YKNWLXL17N",
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
+getAnalytics(app);
 export const db = getFirestore();
